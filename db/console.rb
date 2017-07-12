d0ac1f0b8b4e070b032bcac5a530f07d2b5064e4
staceynapier <staceynapier86@gmail.com>
2017-07-12 19:46:02 +0100

    added method to search albums by artist

--- db/console.rb
+++ db/console.rb
@@ -1,14 +1,19 @@
 require_relative('../models/artist.rb')
 require_relative('../models/album.rb')
+require('pry-byebug')
 
 
 
 artist1 = Artist.new({'name' => 'Meatloaf'})
 
 artist1.save()
-p Artist.all
+# p Artist.all
 
 album1 = Album.new({'title' => 'Bat Out of Hell', 'genre' => 'rock', 'artist_id' => artist1.id})
+album2 = Album.new({'title' => 'Bat Out of Hell2', 'genre' => 'rock', 'artist_id' => artist1.id})
+    # binding.pry
 
 album1.save()
-p Album.all
+album2.save()
+# p Album.all
+p artist1.list_albums()
