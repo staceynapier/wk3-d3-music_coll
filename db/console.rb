ceda65e1dfcefe513d611047493f88b337b22904
staceynapier <staceynapier86@gmail.com>
2017-07-12 16:46:33 +0100

    added find all method to tables

--- db/console.rb
+++ db/console.rb
@@ -6,7 +6,9 @@ require_relative('../models/album.rb')
 artist1 = Artist.new({'name' => 'Meatloaf'})
 
 artist1.save()
+p Artist.all
 
 album1 = Album.new({'title' => 'Bat Out of Hell', 'genre' => 'rock', 'artist_id' => artist1.id})
 
-album1.save
+album1.save()
+p Album.all
