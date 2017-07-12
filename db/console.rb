973e1657588e6a1e6f6d6c032df413701d160da9
Stephen Beattie <beattie19@hotmail.co.uk>
2017-07-12 16:26:40 +0100

    test save method on albums

--- db/console.rb
+++ db/console.rb
@@ -1,7 +1,12 @@
 require_relative('../models/artist.rb')
+require_relative('../models/album.rb')
 
 
 
 artist1 = Artist.new({'name' => 'Meatloaf'})
 
 artist1.save()
+
+album1 = Album.new({'title' => 'Bat Out of Hell', 'genre' => 'rock', 'artist_id' => artist1.id})
+
+album1.save
